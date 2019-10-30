f3971fcdd0340656f98861905db41eca9ea845ad
carlos <anyona6.cj@gmail.com>
2019-10-30 11:22:41 +0300

    create author activity

---
+++ app/src/main/java/com/revosleap/wpdroid/ui/activities/AuthorActivity.kt
@@ -0,0 +1,12 @@
+package com.revosleap.wpdroid.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_author)
+    }
+}
